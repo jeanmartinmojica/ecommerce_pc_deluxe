c84b7f06da215ef8291b4d2c77f231547454e746
jeanmartinmojica <jeanmamojica@gmail.com>
2022-08-25 13:04:28 -0500

    Checkpoint 2

--- javascript/productos.js
+++ javascript/productos.js
@@ -1,22 +1,22 @@
 class Producto{
-    constructor(id, foto, nombre, precio){
+    constructor(id, foto, nombre, precio, precioFinal, cantidad){
         this.id = id
         this.foto = foto
         this.nombre = nombre
         this.precio = precio
+        this.precioFinal = precioFinal
+        this.cantidad = cantidad
     }
 }
 
 const productos =[]
 
 function cargarProductos(){
-    productos.push(new Producto(1, 'assets/Teclados/Alnilam.jpg', "ANILAM MECANICO", 100))
-    productos.push(new Producto(2, 'assets/Teclados/Mintaka.jpg', "MINTAKA MECANICO", 150))
-    productos.push(new Producto(3, 'assets/Teclados/Pulsar.jpg', "PULSAR TKL", 90))
-    productos.push(new Producto(4, 'assets/Teclados/Quasar_RGB.jpg',"QUASAR RGB TKL", 170))
-    productos.push(new Producto(5, 'assets/Audifonos/Arkan.jpg', "ARKAN USB", 140))
-    productos.push(new Producto(6, 'assets/Audifonos/Arkan.jpg' , "GEMINI TRRS 3.5MM", 80))
-    productos.push(new Producto(7, 'assets/Audifonos/Gemini.jpg', "GEMINI TRRS 3.5MM", 75))
-    productos.push(new Producto(8, 'assets/Audifonos/Singularity.jpg', "SINGULARITY USB", 145))
+    productos.push(new Producto(1, 'assets/Teclados/Dragonborn_K630.jpg', "REDRAGON DRAGONBORN K630 ", 100))
+    productos.push(new Producto(2, 'assets/Teclados/Kumara_K552.jpg', "REDRAGON KUMARA K552", 120))
+    productos.push(new Producto(3, 'assets/Teclados/Kumara_K552_blanco.jpg', "REDRAGON KUMARA K552", 120))
+    productos.push(new Producto(5, 'assets/Audifonos/Kotion_Each_G9000.jpg', "KOTION EACH 69000", 90))
+    productos.push(new Producto(6, 'assets/Audifonos/Logitech_Series_G733.jpg' , "LOGITECH SERIES 6733", 150))
+    productos.push(new Producto(7, 'assets/Audifonos/Redragon_Ares.jpg', "REDRAGON ARES", 50))
 }
 cargarProductos()
--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,212 @@
-let carrito=[]
+let carrito = []
+let lista = document.getElementById("milista")
 
-let lista=document.getElementById("milista")
-    
+//llamada a renderizar
+
 renderizarProductos()
 
+//Operador Codicional ternaria
+
+localStorage.getItem("carrito")!=null ?(JSON.parse(localStorage.getItem("carrito")).forEach((producto)=>{agregarAlCarrito(producto)})):""
+    
 function renderizarProductos() {
     for (const producto of productos) {
-        lista.innerHTML+=`<li class="col-md-4 list-group-item">
+        lista.innerHTML += `<li class="col-md-4 list-group-item">
             <h5>${producto.nombre}</h5>
-            <img src=${producto.foto} width="250" height="250">
-            <p><strong> $ ${producto.precio} </strong></p>
-            <button class='btn btn-info' id='btn${producto.id}'>Comprar</button>
+            <img src=${producto.foto} width="300" height="250">
+            <h5>$ ${producto.precio}</h5>
+            <button class='btn btn-info' id='btn${producto.id}'>Agregar al Carrito</button>
         </li>`
     }
-    productos.forEach(producto => {
-        document.getElementById(`btn${producto.id}`).addEventListener('click',function(){
+    //eventos boton
+    productos.forEach((producto) => {
+        //evento para cada boton         
+        let btnAgregarAlCarrito = document.getElementById(`btn${producto.id}`)
+        btnAgregarAlCarrito.addEventListener("click", function () {
             agregarAlCarrito(producto)
+            swal({
+                title: "Agregaste un producto al carrito!",
+                text: `${producto.nombre}`,
+                icon: "success",
+        
+                buttons: ["cerrar", "Ir al carrito"],
+            }).then((irACarrito) => {
+        
+                if (irACarrito) {
+                    const myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+                    const modalToggle = document.getElementById('toggleMyModal')
+                    myModal.show(modalToggle)
+        
+                }
+            })
         })
     })
+
 }
 
-function agregarAlCarrito(producto){
+
+/* funcion calcular Precio final */
+
+function calculcarPrecioFinalCarrito() {
+    let totalCarritoCompra = 0
+    carrito.forEach((producto) => {
+        totalCarritoCompra += producto.precioFinal
+    })
+    return totalCarritoCompra
+}
+
+/* Funcion para dibujar el tfoot con el precio final */
+function dibujarPrecioFinal() {
+    document.getElementById("tablafoot").innerHTML = `
+        <tfoot>
+            <tr>
+                <td> 
+                    Precio Final:
+                </td>
+    
+                <td>
+                    ${calculcarPrecioFinalCarrito()}
+                </td>
+    
+            </tr>
+        </tfoot>
+        
+        `
+      /* Evalua si el carrito esta vacio */
+        if(carrito.length == 0){
+            document.getElementById("tablafoot").innerHTML = `Comienza a comprar`
+            document.querySelector("#modal-foot").innerHTML = `Carrito Vacio`
+            document.getElementById("tablaPrincipal").innerHTML=""
+        
+        }
+        /*Si el carrito no esta vacio dibuja los botones Cerrar Y finalizar compra  */
+        else if(carrito!=0){
+            document.querySelector("#modal-foot").innerHTML = `
+            <div>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Finalizar compra</button>
+    
+            </div>
+            `
+          /* Dibujamos el t head */
+        document.getElementById("tablaPrincipal").innerHTML=`  
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Total</th>
+            </tr>     
+        `     
+
+        }    
+}
+
+    /* Condicion para que al cargar la pagina no muestre el table foot */
+    if(carrito.length == 0){
+    
+        document.querySelector("#modal-foot").innerHTML = `Comience a comprar`
+
+    }
+
+/* ------------------------------------------ */
+function agregarAlCarrito(producto) {
+
+    /* Funcion de orden superior para sweet alert */
+
+    carrito.forEach((el) => {
+        if (el.id == producto.id) {
+            swal({
+                title: "Ya agregaste este producto al carrito!",
+                text: `${producto.nombre}`,
+                icon: "warning",
+                buttons: ["cerrar", "Ir al carrito"],
+            }).then((irACarrito) => {
+
+                if (irACarrito) {
+                    const myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+                    const modalToggle = document.getElementById('toggleMyModal')
+                    myModal.show(modalToggle)
+
+                }
+            })
+            reset
+        }
+    })
+    
+    /* Push de productos en el carrito */
     carrito.push(producto)
-    
-    let cantidad = 1
-    
-    document.getElementById("tablabody").innerHTML+=`
-        <tr>
+    /* Asignacion del valor a nuestra propiedad producto.cantidad */
+    producto.cantidad = 1
+
+    //agrego una fila nueva a la tabla body
+    document.getElementById("tablabody").innerHTML += `
+        <tr id="fila${producto.id}">
             <td>${producto.id}</td>
             <td>${producto.nombre}</td>
-            <td><input type="number" id="cantidad-producto-${producto.id}" value = "${cantidad}"min="1" max="1000" style="width: 50px;"></input></td>
-            <td id="idPrueba">${producto.precio}</td>
+            <td><input type="number" id="cantidad-producto-${producto.id}" value = "${producto.cantidad}"min="1" max="1000" style="width: 50px"></input></td>
+            <td id="valorActual${producto.id}">${producto.precio}</td>
+            <td><button id="eliminar-producto-${producto.id}" type="button" class="btn btn-outline-danger">Eliminar</button></td>             
         </tr>
     `
-    swal({
-        title: "Agregaste un producto al carrito!",
-        text: `${producto.nombre}`,
-        icon: "success",
-        button: "Cerrar",
+
+    /*Asignacion del valor a la propiedad producto.precio final  */
+
+    producto.precioFinal = producto.precio * producto.cantidad
+    
+    /* Funcion que dibuja el precio final al primer click */
+    dibujarPrecioFinal()
+
+
+    /* modifico dinamicamente el total del producto que agregue */
+    carrito.forEach((producto) => {
+        let valorInput = document.getElementById(`cantidad-producto-${producto.id}`)
+        valorInput.addEventListener("change", () => {
+            producto.cantidad = valorInput.value
+            /* Calculo precio Final */
+            let nuevoPrecio = producto.precio * valorInput.value
+            producto.precioFinal = nuevoPrecio
+            document.getElementById(`valorActual${producto.id}`).innerHTML = `
+        <td id="cantidad-producto-${producto.id}">${nuevoPrecio}</td>
+    `
+            dibujarPrecioFinal()
+
+        })
     })
+
+    /* Boton eliminar producto */
+
+    carrito.forEach((producto) => {
+        let btnEliminarProducto = document.getElementById(`eliminar-producto-${producto.id}`)
+        btnEliminarProducto.addEventListener("click", () => {
+            /* document.getElementById(`fila${producto.id}`).innerHTML = "" */ //preguntar por que crea confilcto al cambiar el valor del input y darle click a eliminar
+            eliminarProducto(producto)
+            dibujarPrecioFinal()
+
+        })
+    })
+
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    /* Sweet alert */
+
+}
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+/* Funcion Eliminar productos */
+function eliminarProducto(producto) {
+    let productoAconservar = carrito.filter((productoActual) => productoActual.id != producto.id)
+    carrito.length = 0
+    document.getElementById(`fila${producto.id}`).innerHTML = ""
+    productoAconservar.forEach((productoAgregar) => carrito.push(productoAgregar))
+    //Operador Codicional ternaria
+    carrito.length == 0 ? (document.getElementById("tablabody").innerHTML = ``):''
+    localStorage.setItem("carrito", JSON.stringify(carrito))
 }
